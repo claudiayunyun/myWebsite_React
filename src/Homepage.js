31d5920e2d8f34d556b5662110ac6facb04a385a
Yunyun Huang <claudiayunyun@gmail.com>
2017-11-06 17:25:26 -0800

    change homepage

---
+++ src/Homepage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to Yunyun Website</h1>
+        </header>
+        <p>
+          Have Fun
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
