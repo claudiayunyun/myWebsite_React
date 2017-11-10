cae7f4a798de9bb54d31ba42cbdda82673ddfc3d
Yunyun Huang <claudiayunyun@gmail.com>
2017-11-10 13:53:27 -0800

    change date css

--- src/Homepage/Clock.js
+++ src/Homepage/Clock.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../CSS/clock.css';
 
 class Clock extends Component {
 	constructor() {
@@ -15,14 +16,18 @@ class Clock extends Component {
 	}
 
 	componentWillMount() {
-		setInterval(()=> this.currentTime(), 1000)
+		setInterval(()=> this.currentTime(), 500)
 	}
 
 	render() {
+		const monthArray = ['Jan', 'Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+		const dayArray = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+		const hourArray = ['00','01','02','03','04','05','06','07','08']
 		return (
 			<div>
-				<h4> {this.state.time.toLocaleDateString()} </h4>
-				<h4> {this.state.time.toLocaleTimeString()} </h4>
+				<h4>Local Time:</h4>
+				<h4 className = 'dateString'> {dayArray[this.state.time.getDay()]}, {monthArray[this.state.time.getMonth()]} {this.state.time.getDate()}, {this.state.time.getFullYear()}</h4>
+				<h4> {this.state.time.getHours()} : {this.state.time.getMinutes()} : {this.state.time.getSeconds()}</h4>
 			</div>
 		)
 	}
